c492bb8b64704f6358f7b6eabe1c967505fe7eb4
Komiljon <komiljonrahmatov03@gmail.com>
2024-03-02 00:42:43 +0500

    Added Customer Modul

---
+++ AirportSystem.Domain/Entities/Customer/CustomerCreationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AirportSystem.Domain.Entities.Customer;
+
+public class CustomerCreationModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PassportNumber { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
---
+++ AirportSystem.Domain/Entities/Customer/Customers.cs
@@ -0,0 +1,14 @@
+﻿using AirportSystem.Domain.Commons;
+
+namespace AirportSystem.Domain.Entities.Customer;
+
+public class Customers : Auditable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PassportNumber { get; set; }
+    public string Email {  get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public decimal Balance { get; set; }
+}
