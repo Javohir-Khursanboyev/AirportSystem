9393ad2742bf24ee336a683f9219a62a6de9f228
Komiljon <komiljonrahmatov03@gmail.com>
2024-03-01 23:01:33 +0500

    Add project files.

---
+++ AirportSystem.Domain/Entities/Employee/Employees.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportSystem.Domain.Entities.Employee
+{
+    internal class Employees
+    {
+    }
+}
