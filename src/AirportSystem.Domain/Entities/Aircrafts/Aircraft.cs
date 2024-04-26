02cbce6366c9380a07b2e9c46a387aa9a43bcdd6
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-27 00:04:58 +0500

    Added Aircraft, Airport, Flight, FlightEmployee Entities

---
+++ src/AirportSystem.Domain/Entities/Aircrafts/Aircraft.cs
@@ -0,0 +1,12 @@
+﻿using AirportSystem.Domain.Commons;
+using AirportSystem.Domain.Entities.Flights;
+
+namespace AirportSystem.Domain.Entities.Aircrafts;
+
+public class Aircraft : Auditable
+{
+    public string Name { get; set; }
+    public int TotalOfSeats { get; set; }
+
+    public IEnumerable<Flight> Flights { get; set; }
+}
---
+++ src/AirportSystem.Domain/Entities/Airports/Airport.cs
@@ -0,0 +1,16 @@
+﻿using AirportSystem.Domain.Commons;
+using AirportSystem.Domain.Entities.Flights;
+
+namespace AirportSystem.Domain.Entities.Airports;
+
+public class Airport : Auditable
+{
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string IATACode { get; set; }
+    public string ICAOCode { get; set; }
+
+    public IEnumerable<Flight> StartFlights { get; set; }
+    public IEnumerable<Flight> EndFlights { get; set; }
+}
