bd2ac5a57fb703ca91a6fb8f33c45db28ed5f2f6
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-27 00:51:53 +0500

    Added Exceptions

---
+++ src/AirportSystem.Service/Exceptions/ArgumentIsNotValidException.cs
@@ -0,0 +1,12 @@
+﻿namespace AirportSystem.Service.Exceptions;
+
+public class ArgumentIsNotValidException : Exception
+{
+    public ArgumentIsNotValidException() { }
+
+    public ArgumentIsNotValidException(string message) : base(message) { }
+
+    public ArgumentIsNotValidException(string message, Exception exception) { }
+
+    public int StatusCode => 400;
+}
---
+++ src/AirportSystem.Service/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace AirportSystem.Service.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException() { }
+
+    public NotFoundException(string message) : base(message) { }
+
+    public NotFoundException(string message, Exception exception) { }
+
+    public int StatusCode => 404;
+}
