090daa96d7aa31586a0aa94259f0dd41530ab5c6
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-29 20:31:57 +0500

    Added Payment DTOs

---
+++ src/AirportSystem.Service/DTOs/Payments/PaymentUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿using AirportSystem.Domain.Enums;
+
+namespace AirportSystem.Service.DTOs.Payments;
+
+public class PaymentUpdateModel
+{
+    public long BookingId { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+}
