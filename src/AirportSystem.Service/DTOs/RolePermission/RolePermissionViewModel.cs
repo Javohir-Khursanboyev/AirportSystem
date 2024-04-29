44bdc3fa1caed8e462e1a530c6a14139870ced62
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-29 19:24:15 +0500

    Added Aircraft DTOs

--- src/AirportSystem.Service/DTOs/RolePermission/RolePermissionViewModel.cs
+++ src/AirportSystem.Service/DTOs/RolePermission/RolePermissionViewModel.cs
@@ -5,6 +5,7 @@ namespace AirportSystem.Service.DTOs.RolePermission;
 
 public class RolePermissionViewModel
 {
+    public long Id { get; set; }
     public UserRolesViewModel UserRole { get; set; }
     public PermissionViewModel Permission { get; set; }
 }
