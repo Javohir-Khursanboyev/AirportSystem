1e0256ee02fa13307f948478287284907fa41240
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-29 20:00:30 +0500

    Added Ticket DTOs

---
+++ src/AirportSystem.Service/DTOs/Tickets/TicketViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AirportSystem.Service.DTOs.Tickets;
+
+public class TicketViewModel
+{
+    public long Id { get; set; }
+    public string SeatNumber { get; set; }
+    public long RateTicketsId { get; set; }
+}
