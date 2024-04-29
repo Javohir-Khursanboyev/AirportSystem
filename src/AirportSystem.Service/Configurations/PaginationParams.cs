9143ea87546e5ab22a6374ca0b0236f3f6937488
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-30 00:29:21 +0500

    Read PageIndex and PageSize from appsettings

--- src/AirportSystem.Service/Configurations/PaginationParams.cs
+++ src/AirportSystem.Service/Configurations/PaginationParams.cs
@@ -2,6 +2,14 @@
 
 public class PaginationParams
 {
+    public PaginationParams(int pageIndex, int pageSize)
+    {
+        this.PageIndex = pageIndex;
+        this.PageSize = pageSize;
+    }
+
+    public PaginationParams() { }
+
     public int PageIndex { get; set; } = 1;
     public int PageSize { get; set; } = 20;
 }
---
+++ src/AirportSystem.WebApi/Extensions/ServiceCollection.cs
@@ -0,0 +1,13 @@
+﻿using AirportSystem.Service.Configurations;
+
+namespace AirportSystem.WebApi.Extensions;
+
+public static class ServiceCollection
+{
+    public static void InjectEnvironmentItems(this WebApplication app)
+    {
+        var paginationParams = new PaginationParams
+            (Convert.ToInt32(app.Configuration.GetSection("PaginationParams: PageIndex").Value),
+            Convert.ToInt32(app.Configuration.GetSection("PaginationParams: PageSize").Value));
+    }
+}
