f76c6f7253d90db975fe403e40f20db5b6b2d54d
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-29 23:23:11 +0500

    Added FlightEmployee DTOs

---
+++ src/AirportSystem.Service/DTOs/FlightEmployee/FlightEmployeeUpdateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AirportSystem.Service.DTOs.FlightEmployee;
+
+public class FlightEmployeeUpdateModel
+{
+    public long FlightId { get; set; }
+    public long EmployeeId { get; set; }
+}
