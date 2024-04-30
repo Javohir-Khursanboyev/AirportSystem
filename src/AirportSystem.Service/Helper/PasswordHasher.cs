9e329cc38d8fc5df930c83f171f541ca5f6e65fd
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-30 18:34:54 +0500

    Added PasswordHasher Helper

---
+++ src/AirportSystem.Service/Helper/PasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿namespace AirportSystem.Service.Helper;
+
+public static class PasswordHasher
+{
+    public static string Hash(string password)
+    {
+        var salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+        return BCrypt.Net.BCrypt.HashPassword(password, salt);
+    }
+
+    public static bool Verify(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}
