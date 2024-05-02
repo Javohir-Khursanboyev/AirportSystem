d27bd2dcfb39580c7b0e5737e4fde3336f0ec2e1
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-02 10:31:22 +0500

    Added CollectionExtension

---
+++ src/AirportSystem.Service/Extensions/CollectionExtension.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using AirportSystem.Service.Helper;
+using AirportSystem.Service.Configurations;
+using AirportSystem.Service.Exceptions;
+using System.Linq.Expressions;
+
+namespace AirportSystem.Service.Extensions;
+
+public static class CollectionExtension
+{
+    public static IQueryable<T> ToPaginateAsQueryable<T>(this IQueryable<T> source, PaginationParams @params)
+    {
+        int totalCount = source.Count();
+
+        var json = JsonConvert.SerializeObject(new PaginationMetaData(totalCount, @params));
+
+        HttpContextHelper.ResponseHeaders.Remove("X-Pagination");
+        HttpContextHelper.ResponseHeaders?.Add("X-Pagination", json);
+
+        return @params.PageIndex > 0 &&  @params.PageSize > 0
+            ? source.Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize)
+            : source;
+    }
+
+    public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, Filter filter)
+    {
+        var expression = source.Expression;
+
+        var parameter = Expression.Parameter(typeof(T), "x");
+
+        MemberExpression selector;
+        try
+        {
+            selector = Expression.PropertyOrField(parameter, filter?.OrderBy ?? "Id");
+        }
+        catch
+        {
+            throw new ArgumentIsNotValidException("Specified property is not found");
+        }
+
+        var method = string.Equals(filter?.OrderType ?? "asc", "desc", StringComparison.OrdinalIgnoreCase) ? "OrderByDescending" : "OrderBy";
+
+        expression = Expression.Call(typeof(Queryable), method,
+            new Type[] { source.ElementType, selector.Type },
+            expression, Expression.Quote(Expression.Lambda(selector, parameter)));
+
+        return source.Provider.CreateQuery<T>(expression);
+    }
+}
